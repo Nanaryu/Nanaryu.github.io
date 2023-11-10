a1dee3072795c15ee15854e37aaabafc6d482c7a
Nanaryu <filipqqq1234@gmail.com>
2023-11-10 15:20:44 +0100

    added power train and visually updated the result

--- script.js
+++ script.js
@@ -49,17 +49,55 @@ function auto_min_raw_damage_Calc(stat, weaponatk, base)
   return auto_min_raw_damage
 }
     
-
 function auto_max_raw_damage_Calc(stat, weaponatk, base)
 {
   return (stat * weaponatk)/10 + (base)/4
 }
+
+function special_meldist_min_raw_damage_Calc(stat, weaponatk, base)
+{
+  return 1.5*((stat * weaponatk)/20 +(base)/4)
+}
+
+function special_meldist_max_raw_damage_Calc(stat, weaponatk, base)
+{
+  return 1.5*((stat * weaponatk)/10 +(base)/4)
+}
+
+function special_magic_min_raw_damage_Calc(stat, weaponatk, base)
+{
+  return 1.5*(((1.05*(stat) * weaponatk)/20) + 9*(base)/32)
+}
+
+function special_magic_max_raw_damage_Calc(stat, weaponatk, base)
+{
+  return 1.5*(((1.05*(stat) * weaponatk)/10) + 9*(base)/32)
+}
     
+function min_damage_Calc(min_raw_damage, pos)
+{
+  var min_damage = min_raw_damage - mobs[pos][1]
+  if (min_damage < 0){
+    min_damage = 0
+  }
+  return min_damage
+}
+
+function max_damage_Calc(max_raw_damage, pos)
+{
+  return max_raw_damage - mobs[pos][1]
+}
+
 function max_raw_crit_damage_Calc(max_raw_damage)
 {
   return max_raw_damage * 1.05
 }
-    
+
+function max_crit_damage_Calc(max_raw_crit_damage, pos)
+{
+  return max_raw_crit_damage - mobs[pos][1]
+}
+  
 function normal_accuracy_Calc(max_raw_damage, min_raw_damage, x)
 {
   var normalaccuracy = (max_raw_damage-mobs[x][1])/(max_raw_damage - min_raw_damage)
@@ -68,7 +106,6 @@ function normal_accuracy_Calc(max_raw_damage, min_raw_damage, x)
   return normalaccuracy
 }
     
-
 function crit_accuracy_Calc(max_raw_crit_damage, max_raw_damage, x)
 {
   var critaccuracy = (max_raw_crit_damage-mobs[x][1])/(max_raw_crit_damage - max_raw_damage)
@@ -77,10 +114,14 @@ function crit_accuracy_Calc(max_raw_crit_damage, max_raw_damage, x)
   return critaccuracy
 }
     
-
 function accuracy_Calc(max_raw_crit_damage, max_raw_damage, min_raw_damage, x)
 {
   return (normal_accuracy_Calc(max_raw_damage, min_raw_damage, x)*0.99) + (crit_accuracy_Calc(max_raw_crit_damage, max_raw_damage, x)*0.01)
+}
+
+function total_accuracy_Calc(accuracy, tick)
+{
+  return 1.0 - Math.pow(Math.pow(1.0-accuracy,tick),10)
 }
 
 function average_damage_Calc(accuracy, max_damage, min_damage, max_crit_damage){
@@ -97,23 +138,64 @@ function tickrate_Calc(accuracy, maxtickrate)
   return maxtickrate*(1.0 - Math.pow(1.0-accuracy,10.0))
 }
 
-function min_damage_Calc(min_raw_damage, pos)
+function powertickrate_Calc(totalaccuracy, maxtickrate)
 {
-  var min_damage = min_raw_damage - mobs[pos][1]
-  if (min_damage < 0){
-    min_damage = 0
+  return maxtickrate*totalaccuracy
+}
+
+function max_tickrate_Calc(tick)
+{
+  if (tick <= 5) {
+      return tick * 3600
+  } else {
+      return 18000
   }
-  return min_damage
 }
 
-function max_damage_Calc(max_raw_damage, pos)
+function threshold_Calc(tick)
 {
-  return max_raw_damage - mobs[pos][1]
+  return 1.0 - Math.pow(.8251,(1.0/tick))
 }
 
-function max_crit_damage_Calc(max_raw_crit_damage, pos)
+
+var ptrain_state = false
+var ptrainbox = document.getElementById("ptrain")
+function ptrain()
 {
-  return max_raw_crit_damage - mobs[pos][1]
+  if (!ptrain_state)
+  {
+    ptrainbox.style.backgroundColor = "rgb(60, 163, 31)"
+    ptrainbox.style.borderColor = "rgb(60, 163, 31)"
+    ptrainbox.placeholder = "Ptrain: On"
+    ptrain_state = true
+  }
+  else
+  {
+    ptrainbox.style.backgroundColor = "rgb(100, 100, 100)"
+    ptrainbox.style.borderColor = "rgb(100, 100, 100)"
+    ptrainbox.placeholder = "Ptrain: Off"
+    ptrain_state = false
+  }
+}
+
+var mage_state = false
+var magebox = document.getElementById("mage")
+function mage()
+{
+  if (!mage_state)
+  {
+    magebox.style.backgroundColor = "rgb(60, 163, 31)"
+    magebox.style.borderColor = "rgb(60, 163, 31)"
+    magebox.placeholder = "Mage: On"
+    mage_state = true
+  }
+  else
+  {
+    magebox.style.backgroundColor = "rgb(100, 100, 100)"
+    magebox.style.borderColor = "rgb(100, 100, 100)"
+    magebox.placeholder = "Mage: Off"
+    mage_state = false
+  }
 }
 
 
@@ -126,10 +208,13 @@ function train()
   let str4 = "" //You need...
   let str5 = "" //You deal...
 
-
   let weaponatk = parseFloat(document.getElementById("weaponatk").value)
   let base = parseFloat(document.getElementById("base").value)
   let stat = parseFloat(document.getElementById("stat").value)
+
+  if (!ptrain_state)
+  { // ===================================IF NOT PTRAIN BRACKET=====================================================
+
   let min_raw_damage = auto_min_raw_damage_Calc(stat, weaponatk, base)
   let max_raw_damage = auto_max_raw_damage_Calc(stat, weaponatk, base)
   let max_raw_crit_damage = max_raw_crit_damage_Calc(max_raw_damage)
@@ -154,9 +239,9 @@ function train()
   let tickrate = tickrate_Calc(accuracy, 3600)
   
 
-  var onemob = true;
-  var checknextmob = true;
-  var newpos = pos + 1;
+  var onemob = true
+  var checknextmob = true
+  var newpos = pos + 1
 
   if (pos == 5 || pos == 20 || pos == 22 || pos == 28 || pos == 30) 
   {
@@ -167,22 +252,205 @@ function train()
   {
     checknextmob = false
   }
-  if (newpos == 26||newpos == 31)
+  if (newpos == 26 || newpos == 31)
   {
     newpos++
   }
 
-  var kill_time = time_to_kill(avgdmg, pos)
+  var time = time_to_kill(avgdmg, pos)
 
-  str0 = `You can train effectively on ${mobs[pos][0]}.`
-  str3 = `Average time to kill ${mobs[pos][0]}: ${Math.round(kill_time/60)} min. ${Math.round(kill_time%60)} sec.`
+  str0 = `You can train effectively on <span class="info">${mobs[pos][0]}</span>.`
   if (!onemob)
   {
-    kill_time = time_to_kill(avgdmg, pos+1)
-    str0 = `You can train effectively on ${mobs[pos][0]} & ${mobs[pos+1][0]}.`
-    str3 = `Average time to kill ${mobs[pos+1][0]}: ${Math.round(kill_time/60)} min. ${Math.round(kill_time%60)} sec.`
+    let time2 = time_to_kill(avgdmg, pos+1)
+    str0 = `You can train effectively on <span class="info">${mobs[pos][0]}</span> & <span class="info">${mobs[pos+1][0]}</span>.`
+    str3 = `Average time to kill <span class="info">${mobs[pos+1][0]}</span>: <span class="time">${Math.round(time2/60)} min. ${Math.round(time2%60)} sec.</span>`
+  }
+
+  let statadd = 0
+  let checked = false
+  let alrdealdamage = false
+  let dealdamage = false
+  let new_max_damage
+  let newaccuracy = 0
+
+  while (newaccuracy < 0.1749 && checknextmob) 
+  {
+      let statneeded = stat + statadd
+
+      let new_min_raw_damage = auto_min_raw_damage_Calc(statneeded, weaponatk, base)
+      let new_max_raw_damage = auto_max_raw_damage_Calc(statneeded, weaponatk, base)
+
+      let new_max_raw_critdamage = max_raw_crit_damage_Calc(new_max_raw_damage)
+
+      new_max_damage = max_damage_Calc(new_max_raw_damage, newpos)
+      newaccuracy = accuracy_Calc(new_max_raw_critdamage, new_max_raw_damage, new_min_raw_damage, newpos);
+
+      if (new_max_damage >= 1 && !checked)  //if you can already deal damage to the next mob
+      {
+          str5 = `You can deal <span class="numbers">${Math.round(new_max_damage)}</span> max damage to <span class="info">${mobs[newpos][0]}</span>!` //part of output
+          alrdealdamage = true
+      }
+      else if (new_max_damage > 1 && !alrdealdamage && !dealdamage) //if you cant deal damage to the next mob yet, you can deal damage in a certain amount of stats!
+      { 
+          str5 = `You can deal <span class="numbers">${Math.round(new_max_damage)}</span> max damage to <span class="info">${mobs[newpos][0]}</span> in <span class="numbers">${statadd}</span> stats!` //part of output
+          dealdamage = true
+      }
+      checked = true
+      statadd++
+  }
+  if (checknextmob) 
+  {
+      str1 = `Max. Damage: <span class="numbers">${Math.round(max_damage)}</span> Tickrate: <span class="numbers">${Math.round(tickrate)}</span> / <span class="numbers">3600</span>`
+      str2 = `Average time to kill <span class="info">${mobs[pos][0]}</span>: <span class="time">${Math.round(time/60)} min. ${Math.round(time%60)} sec.</span>`
+      str4 = `You need <span class="numbers">${statadd}</span> stats to train effectively on <span class="info">${mobs[newpos][0]}</span>!`
+  }
+  else 
+  {
+      str1 = `Min. Damage (Auto): ${Math.round(min_damage)} Max. Damage (Auto): ${Math.round(max_damage)}`
+      str2 = `Average time to kill ${mobs[pos][0]}: ${Math.round(time/60)} min. ${Math.round(time%60)} sec.`
   }
   
+  if (str3 === "")
+  {
+    document.getElementById("result").innerHTML = str0 + "<br>" + str1 + "<br>" + str2 + "<br>" + str4 + "<br>" + str5
+  }
+  else
+  {
+    document.getElementById("result").innerHTML = str0 + "<br>" + str1 + "<br>" + str2 + "<br>" + str3 + "<br>" + str4 + "<br>" + str5
+  }
 
-  document.getElementById("result").innerHTML = str0 + "<br>" + str3 + "<br>" + `Tickrate: ${Math.round(tickrate)}/3600.`
+  } // ===================================IF NOT PTRAIN BRACKET=====================================================
+  else
+  { // =====================================IF PTRAIN BRACKET=======================================================
+    let max_raw_damage
+    let min_raw_damage
+
+    if (mage_state) 
+    {
+      min_raw_damage = special_magic_min_raw_damage_Calc(stat, weaponatk, base)
+      max_raw_damage = special_magic_max_raw_damage_Calc(stat, weaponatk, base)
+    } 
+    else 
+    {
+      min_raw_damage = special_meldist_min_raw_damage_Calc(stat, weaponatk, base)
+      max_raw_damage = special_meldist_max_raw_damage_Calc(stat, weaponatk, base)
+    }
+
+    let max_raw_crit_damage = max_raw_crit_damage_Calc(max_raw_damage)
+    let accuracy = 0
+    let pos = 0
+    let tick = 4
+    let threshold = threshold_Calc(tick)
+
+    //Iterate through loop until you find a mob you can power train on with greater than .1749 accuracy
+    for (let x = mobs.length-1; x >= 0; x--) 
+    {
+      if (x == 13 || x == 19 || x == 20 || x == 22 || x == 24 || x == 25 || x == 26 || x == 29 || x == 31 || x == 33 || x == 36 || x == 37 || x >= 39) 
+      {
+        continue
+      }
+
+      accuracy = accuracy_Calc(max_raw_crit_damage, max_raw_damage, min_raw_damage, x)
+
+      if (accuracy >= threshold) 
+      {
+          pos = x
+          break
+      }
+    }
+
+    //Calculate average damage which you need for average time to kill
+    let min_damage = min_damage_Calc(min_raw_damage, pos)
+    let max_damage = max_damage_Calc(max_raw_damage, pos)
+    let max_crit_damage = max_crit_damage_Calc(max_raw_crit_damage, pos)
+    let avgdmg = average_damage_Calc(accuracy, max_damage, min_damage, max_crit_damage)
+    let totalaccuracy = total_accuracy_Calc(accuracy, tick)
+    let maxtickrate = max_tickrate_Calc(tick)
+    let powertickrate = powertickrate_Calc(totalaccuracy, maxtickrate)
+    let time = time_to_kill(avgdmg, pos)
+
+    str0 = `You can power train on on <span class="info">${mobs[pos][0]}</span>.`
+
+    //calculating stats you need to train on the next mob
+
+    let statadd = 0
+    let checked = false
+    let alrdealdamage = false
+    let dealdamage = false
+    let new_max_damage
+    let newaccuracy = 0
+    let newpos = pos + 1
+
+    while (true) 
+    {
+        if (newpos == 13 || newpos == 19 || newpos == 20 || newpos == 22 || newpos == 24 || newpos == 25 || newpos == 26 || newpos == 29 || newpos == 31 || newpos == 33 || newpos == 36 || newpos == 37) 
+        {
+            newpos++
+        }
+        else if (newpos >= 38) 
+        {
+            newpos = 38
+            break
+        }
+        else 
+        {
+            break
+        }
+    }
+
+    let checknextmob = pos != 38
+
+    while (newaccuracy < threshold && checknextmob) 
+    {
+      let statneeded = stat + statadd
+
+      let new_max_raw_damage
+      let new_min_raw_damage
+
+        if (mage_state) 
+        {
+            new_min_raw_damage = special_magic_min_raw_damage_Calc(statneeded, weaponatk, base)
+            new_max_raw_damage = special_magic_max_raw_damage_Calc(statneeded, weaponatk, base)
+        } 
+        else 
+        {
+            new_min_raw_damage = special_meldist_min_raw_damage_Calc(statneeded, weaponatk, base)
+            new_max_raw_damage = special_meldist_max_raw_damage_Calc(statneeded, weaponatk, base)
+        }
+
+        let new_max_raw_crit_damage = max_raw_crit_damage_Calc(new_max_raw_damage)
+
+        new_max_damage = max_damage_Calc(new_max_raw_damage, newpos)
+
+        newaccuracy = accuracy_Calc(new_max_raw_crit_damage, new_max_raw_damage, new_min_raw_damage, newpos)
+        
+        if (new_max_damage >= 1 && !checked) //if you can already deal damage to the next mob
+        { 
+            str4 = `You can deal <span class="numbers">${Math.round(new_max_damage)}</span> max damage to <span class="info">${mobs[newpos][0]}</span>!` //part of output
+            alrdealdamage = true
+        }
+        else if (new_max_damage > 1 && !alrdealdamage && !dealdamage) //if you cant deal damage to the next mob yet, you can deal damage in a certain amount of stats!
+        { 
+            str4 = `You can deal <span class="numbers">${Math.round(new_max_damage)}</span> max damage to <span class="info">${mobs[newpos][0]}</span> in <span class="numbers">${statadd}</span> stats!` //part of output
+            dealdamage = true
+        }
+        checked = true
+        statadd++
+    }
+
+    //Building remaining Strings
+    if (checknextmob) 
+    {
+        str1 = `Max. Damage: <span class="numbers">${Math.round(max_damage)}</span> Tickrate: <span class="numbers">${Math.round(powertickrate)}</span> / <span class="numbers">${Math.round(maxtickrate)}</span>`
+        str2 = `Average time to kill <span class="info">${mobs[pos][0]}</span>: <span class="time">${Math.round(time/60)} min. ${Math.round(time%60)} sec.</span>`
+        str3 = `You need <span class="numbers">${statadd}</span> stats to power train effectively on <span class="info">${mobs[newpos][0]}</span>!`
+    }
+    else 
+    {
+        str1 = `Min. Damage (Auto): ${Math.round(min_damage)} Max. Damage (Auto): ${Math.round(max_damage)}`
+        str2 = `Average time to kill ${mobs[pos][0]}: ${Math.round(time/60)} min. ${Math.round(time%60)} sec.`
+    }
+    document.getElementById("result").innerHTML = str0 + "<br>" + str1 + "<br>" + str2 + "<br>" + str3 + "<br>" + str4
+  } // =====================================IF PTRAIN BRACKET=======================================================
 }
